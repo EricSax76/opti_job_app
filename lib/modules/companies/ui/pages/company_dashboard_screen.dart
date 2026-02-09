f149d4cbd4895c32f4bd3e2c71ba8f0469fa6fda
Eric Moscoso <ericmoscoso76@hotmail.com>
2026-02-09 18:03:43 +0100

    candidates refactor

--- lib/modules/companies/ui/pages/company_dashboard_screen.dart
+++ lib/modules/companies/ui/pages/company_dashboard_screen.dart
@@ -5,12 +5,12 @@ import 'package:opti_job_app/core/theme/theme_cubit.dart';
 import 'package:opti_job_app/core/theme/theme_state.dart';
 import 'package:opti_job_app/auth/ui/pages/unauthenticated_company_message.dart';
 import 'package:opti_job_app/modules/companies/cubits/company_auth_cubit.dart';
-import 'package:opti_job_app/modules/companies/ui/pages/company_candidates_tab.dart';
+import 'package:opti_job_app/modules/companies/ui/widgets/company_candidates_tab.dart';
 import 'package:opti_job_app/modules/companies/ui/pages/company_home_dashboard.dart';
 import 'package:opti_job_app/modules/companies/ui/widgets/company_offer_creation_tab.dart';
 import 'package:opti_job_app/modules/companies/ui/widgets/offers/company_offers_tab.dart';
 import 'package:opti_job_app/modules/companies/ui/widgets/company_account_avatar_menu.dart';
-import 'package:opti_job_app/modules/companies/ui/pages/company_dashboard_nav_bar.dart';
+import 'package:opti_job_app/modules/companies/ui/widgets/company_dashboard_nav_bar.dart';
 import 'package:opti_job_app/modules/job_offers/cubits/company_job_offers_cubit.dart';
 import 'package:opti_job_app/modules/job_offers/cubits/job_offer_form_cubit.dart';
 
@@ -89,7 +89,8 @@ class _CompanyDashboardScreenState extends State<CompanyDashboardScreen>
                       return IconButton(
                         icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
                         tooltip: isDark ? 'Modo claro' : 'Modo oscuro',
-                        onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+                        onPressed: () =>
+                            context.read<ThemeCubit>().toggleTheme(),
                       );
                     },
                   ),
